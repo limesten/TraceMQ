66f7c7bf2b3aa75d5d33315233f4842c14dffa9e
Emil Malmsten <emil_malmsten@hotmail.com>
2026-09-09 18:29:25 +0200

    embed react

--- src/TraceMQ.Api/Program.cs
+++ src/TraceMQ.Api/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Worldz!");
 app.MapGet("/api/ping", () => new { message = "pong", at = DateTimeOffset.Now });
+
+var files = new ManifestEmbeddedFileProvider(Assembly.GetExecutingAssembly(), "wwwroot");
+var opts = new StaticFileOptions { FileProvider = files };
+
+app.UseDefaultFiles(new DefaultFilesOptions { FileProvider = files });
+app.UseStaticFiles(opts);
+app.MapFallbackToFile("index.html", opts);
 
 app.Run();
